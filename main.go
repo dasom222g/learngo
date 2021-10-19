5bce4d7b0785eb1e295a7a101c2107d82ae241f0
dasom <dasom228@gmail.com>
2021-10-19 18:37:15 +0900

    Add const and var

--- main.go
+++ main.go
@@ -3,5 +3,10 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Hello world!")
+	fmt.Println("Hello")
+	// const name string = "dasomi"
+	// var name string = "dasomi"
+	name := "dasomi"
+
+	fmt.Println("name " + name)
 }
